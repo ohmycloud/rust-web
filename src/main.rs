65992ec6d96a43743e30d5adb986c8d743670bf3
ohmycloud <ohmycloudy@gmail.com>
2024-03-30 21:53:58 +0800

    read from json file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
+use std::fmt::Formatter;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Question {
@@ -51,26 +52,74 @@ pub async fn get_questions(
     params: HashMap<String, String>,
     store: Store
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    println!("{:?}", params);
-    let res: Vec<Question> = store.questions.values().cloned().collect();
-    Ok(warp::reply::json(&res))
+    if !params.is_empty() {
+        let pagination = extract_pagination(params)?;
+        let res: Vec<Question> = store.questions.values().cloned().collect();
+        let res = &res[pagination.start .. pagination.end];
+        Ok(warp::reply::json(&res))
+    } else {
+        let res: Vec<Question> = store.questions.values().cloned().collect();
+        Ok(warp::reply::json(&res))
+    }
 }
 
 pub async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
     println!("{:?}", r);
-    if let Some(_error) = r.find::<CorsForbidden>() {
+    if let Some(error) = r.find::<Error>() {
         Ok(warp::reply::with_status(
-            "Cors Forbidden",
+            error.to_string(),
+            StatusCode::RANGE_NOT_SATISFIABLE,
+        ))
+    } else if let Some(error) = r.find::<CorsForbidden>() {
+        Ok(warp::reply::with_status(
+            error.to_string(),
             StatusCode::FORBIDDEN,
         ))
     } else {
         Ok(warp::reply::with_status(
-            "Route not found",
+            "Route not found".to_string(),
             StatusCode::NOT_FOUND,
         ))
     }
 }
 
+#[derive(Debug)]
+enum Error {
+    ParseError(std::num::ParseIntError),
+    MissingParameters,
+}
+
+#[derive(Debug)]
+struct Pagination {
+    start: usize,
+    end: usize,
+}
+
+pub fn extract_pagination(
+    params: HashMap<String, String>) -> Result<Pagination, Error> {
+    if params.contains_key("start") && params.contains_key("end") {
+        return Ok(
+            Pagination {
+                start: params.get("start").unwrap().parse::<usize>()
+                    .map_err(Error::ParseError)?,
+                end: params.get("end").unwrap().parse::<usize>()
+                    .map_err(Error::ParseError)?,
+            });
+    }
+    Err(Error::MissingParameters)
+}
+
+impl Reject for Error {}
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            Error::ParseError(ref err) => {
+                write!(f, "Cannot parse parameter: {}", err)
+            },
+            Error::MissingParameters => write!(f, "Missing parameter"),
+        }
+    }
+}
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let store = Store::new();
