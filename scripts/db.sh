095ca568877908792dba55fb1481237aa4c43e7e
ohmycloud <ohmycloudy@gmail.com>
2024-04-05 21:22:48 +0800

    add sqlx-cli command

--- scripts/db.sh
+++ scripts/db.sh
@@ -1,1 +1,11 @@
 psql -h localhost -p 5432 -u postgres
+
+# create
+sqlx migrate add -r question_table
+sqlx migrate run --database-url postgresql://postgres:password@localhost:5432/rustwebdev
+sqlx migrate add -r answer_table
+sqlx migrate run --database-url postgresql://postgres:password@localhost:5432/rustwebdev
+
+# revert
+sqlx migrate revert --database-url postgresql://postgres:password@localhost:5432/rustwebdev
+sqlx migrate revert --database-url postgresql://postgres:password@localhost:5432/rustwebdev
--- sql/answers.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS answers (
-    id serial PRIMARY KEY,
-    content TEXT NOT NULL,
-    created_on TIMESTAMP NOT NULL DEFAULT NOW(),
-    corresponding_question integer REFERENCES questions
-);
