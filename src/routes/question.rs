39fe6b4433288f4d90c9644341133efe6c4e2e82
ohmycloud <ohmycloudy@gmail.com>
2024-04-14 11:03:02 +0800

    add parallel execution for update_question

--- src/routes/question.rs
+++ src/routes/question.rs
@@ -21,20 +21,27 @@ pub async fn update_question(
     store: Store,
     question: Question,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    let title = match check_profanity(question.title).await {
-        Ok(res) => res,
-        Err(e) => return Err(warp::reject::custom(e)),
-    };
+    // let title = match check_profanity(question.title).await {
+    //     Ok(res) => res,
+    //     Err(e) => return Err(warp::reject::custom(e)),
+    // };
+    let title = tokio::spawn(check_profanity(question.title));
+    let content = tokio::spawn(check_profanity(question.content));
 
-    let content = match check_profanity(question.content).await {
-        Ok(res) => res,
-        Err(e) => return Err(warp::reject::custom(e)),
-    };
+    let (title, content) = (title.await.unwrap(), content.await.unwrap());
+
+    if title.is_err() {
+        return Err(warp::reject::custom(title.unwrap_err()));
+    }
+
+    if content.is_err() {
+        return Err(warp::reject::custom(content.unwrap_err()));
+    }
 
     let question = Question {
       id: question.id,
-        title,
-        content,
+        title: title.unwrap(),
+        content: content.unwrap(),
         tags: question.tags
     };
 
