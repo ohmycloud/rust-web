634fbb3102ce8e5a4e119ec02238041c62729c9a
ohmycloud <ohmycloudy@gmail.com>
2024-04-14 16:06:37 +0800

    hash+salt password

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -1,11 +1,32 @@
 use warp::http::StatusCode;
 use crate::store::Store;
 use crate::types::account::Account;
+use argon2::{self, Config};
+use rand::random;
+
+// The hash function returns a string, the hashed version of the clear-text password
+pub fn hash_password(password: &[u8]) -> String {
+    // The rand function creates s32 random bytes and stores theme in a slice
+    let salt = random::<[u8; 32]>();
+    // Argon2 depends on a configuration, and we will use the default set.
+    let config = Config::default();
+    // With the password, the salt, and the config, we can hash our clear-text password.
+    argon2::hash_encoded(password, &salt, &config).unwrap()
+}
 
 pub async fn register(
     store: Store,
     account: Account
 ) -> Result<impl warp::Reply, warp::Rejection> {
+    // Takes the password as a byte array and passes it to the newly created hash function
+    let hashed_password = hash_password(account.password.as_bytes());
+
+    let account = Account {
+        id: account.id,
+        email: account.email,
+        password: hashed_password,
+    };
+
     match store.add_account(account).await {
         Ok(_) => {
             Ok(warp::reply::with_status("Account added", StatusCode::OK))
